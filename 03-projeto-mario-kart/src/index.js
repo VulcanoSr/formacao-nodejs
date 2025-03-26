7d512b943367abdcc2bb09b910f09afba1eb3aa6
terukimateus <terukimateus@outlook.com>
2025-03-26 15:28:32 -0300

    Adicionado todos os personagens

--- 03-projeto-mario-kart/src/index.js
+++ 03-projeto-mario-kart/src/index.js
@@ -14,6 +14,35 @@ const player2 = {
   PONTOS: 0,
 };
 
+const player3 = {
+  NOME: "Peach",
+  VELOCIDADE: 3,
+  MANOBRABILIDADE: 4,
+  PODER: 2,
+  PONTOS: 0,
+};
+const player4 = {
+  NOME: "Yoshi",
+  VELOCIDADE: 2,
+  MANOBRABILIDADE: 4,
+  PODER: 3,
+  PONTOS: 0,
+};
+const player5 = {
+  NOME: "Bowser",
+  VELOCIDADE: 5,
+  MANOBRABILIDADE: 2,
+  PODER: 5,
+  PONTOS: 0,
+};
+const player6 = {
+  NOME: "Donkey Kong",
+  VELOCIDADE: 2,
+  MANOBRABILIDADE: 2,
+  PODER: 5,
+  PONTOS: 0,
+};
+
 async function rollDice() {
   return Math.floor(Math.random() * 6) + 1;
 }
